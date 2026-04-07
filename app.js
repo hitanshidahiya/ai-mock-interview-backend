e6dd2c6b1842530703e42518dda2f19ec044cd32
Hitanshi <hitanshidahiya76@gmail.com>
2026-04-08 00:23:31 +0530

    updated / root

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/interview", interviewRoutes);
 app.use("/api/dashboard", dashboardRoutes);
